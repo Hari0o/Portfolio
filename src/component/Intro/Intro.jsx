a76172cb0c3f530aa48a42651774cc0dcfed00a1
Hari0o <harisrikanth0@gmail.com>
2024-06-26 12:09:06 +0530

    latest

--- src/component/Intro/Intro.jsx
+++ src/component/Intro/Intro.jsx
@@ -12,34 +12,39 @@ function Intro() {
 
    
     return (
-        <div className="intro" id='intro'>
-            <div className="left">
-                <div className="imgContainer">
-                    <img src="assets/hari.png" alt="" />
-                </div>
-                
-            </div>
-            <div className="right">
-                <div className="details">
-                    <h2>Hey There,I'm</h2>
-                    <h1>Hari Mukkamula</h1>
-                    <h3>Web Developer <span ref={textRef}></span> </h3>
-
-                    <div className="box">
-                    <h3 className='p'>Profile</h3>
-                    <p>Proficient in an assortment of technologies like React and JavaScript.
-                        Able to effectively self-manage during independent projects, as well as
-                        collaborate in a team setting.</p>
-                        </div>
-                    
-                </div>
-                <a href="#porto">
-                    <img src="assets/down.png" alt="" />
-                    </a>
-            </div>
-
+      <div className="intro" id="intro">
+        <div className="left">
+          <div className="imgContainer">
+            <img src="assets/hari.png" alt="" />
+          </div>
         </div>
-    )
+        <div className="right">
+          <div className="details">
+            <h2>Hey There,I'm</h2>
+            <h1>Hari Mukkamula</h1>
+            <h3>
+              Web Developer <span ref={textRef}></span>{" "}
+            </h3>
+
+            <div className="box">
+              <h3 className="p">Profile</h3>
+              <p>
+                Proficient in an assortment of technologies like HTMl, CSS,
+                JavaScript, React and Deployments by using Docker, Kubernets
+                implementing CI/CD pipelines using tools like Jenkins and Skilled in
+                leveraging Terraform and Ansible for infrastructure as code
+                (IaC) and configuration management. Able to effectively
+                self-manage during independent projects, as well as collaborate
+                in a team setting.
+              </p>
+            </div>
+          </div>
+          <a href="#porto">
+            <img src="assets/down.png" alt="" />
+          </a>
+        </div>
+      </div>
+    );
 }
 
 export default Intro;
